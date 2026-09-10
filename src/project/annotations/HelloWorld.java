8bd44d4ff07ca5be185738ac35a29cbde85a2396
descurra <descurra2005@gmail.com>
2026-09-10 12:56:44 -0400

    Update HelloWorld.java

--- src/project/annotations/HelloWorld.java
+++ src/project/annotations/HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld{
-    public Static void main(String[] args){
-        System.out.println("Hello World");
-    }
+  public Static void main(String[] args){
+      System.out.println("Hello World");
+  }
 }
